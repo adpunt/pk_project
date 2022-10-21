5849843eeb8e49589c254f415011db5cf2c1d85e
adpunt <apunt@hmc.edu>
2022-10-20 21:14:06 -0700

    Update Protocol to take in initial dose, dosing function, and timespan

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -12,6 +12,25 @@ class Protocol:
         an example paramter
 
     """
-    def __init__(self, value=43):
-        self.value = value
+    def __init__(self, initial_dose: float, time: float, dose_func=lambda x, y: 0):
+        if not callable(dose_func):
+            raise TypeError('dose_func must be a callable function.')
+        try: 
+            # Test out dose_func to see if given two numeric inputs, it returns a numeric output
+            val = dose_func(1, 2)
+            float(val)
+        except:
+            raise TypeError('dose_func must take in two numeric inputs and return a numeric output.')
+        self.dose_func = dose_func
+        try:
+            self.initial_dose = float(initial_dose)
+            self.time = float(time)
+        except ValueError:
+            raise TypeError('initial_dose and time must be numeric.')
 
+    def __str__(self):
+        return self.name 
+
+    @property
+    def name(self) -> str:
+        return '[initial_dose={0}, time={1}]'.format(self.initial_dose, self.time)
